19dfb30e25df2bd294da6d723e6aa2bb8a26a185
Madhanbabus293 <46224185+Madhanbabus293@users.noreply.github.com>
2019-09-03 17:40:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Example from './App';
+// import Func from './next'
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Example/>, document.getElementById('root'));
+// ReactDOM.render(<Func/>, document.getElementById('root'));
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
